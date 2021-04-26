19625317a072c4bb1df7860eca31cc3c386c5d93
DaianaKaneshiro <daiana.kane@gmail.com>
2021-04-26 18:42:19 +0900

    Con Aplicacion parcial

--- src/Spec.hs
+++ src/Spec.hs
@@ -5,7 +5,22 @@ import Test.Hspec
 
 correrTests :: IO ()
 correrTests = hspec $ do
-  describe "Test de ejemplo" $ do
+  describe "presionDeGolpe" $ do
     it "El pdepreludat se instaló correctamente" $ do
       doble 1 `shouldBe` 2
 
+  describe "gomuGomu" $ do
+    it "El pdepreludat se instaló correctamente" $ do
+      doble 1 `shouldBe` 2
+
+  describe "golpesNormales" $ do
+    it "El pdepreludat se instaló correctamente" $ do
+      doble 1 `shouldBe` 2
+
+  describe "objetivoDificil" $ do
+    it "El pdepreludat se instaló correctamente" $ do
+      doble 1 `shouldBe` 2
+
+  describe "objetivoAccesible" $ do
+    it "El pdepreludat se instaló correctamente" $ do
+      doble 1 `shouldBe` 2
